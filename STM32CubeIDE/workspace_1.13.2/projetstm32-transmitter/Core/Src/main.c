caeae7ac499bfe551f5d6f8d462d4b954e858362
mohamed ammar bellalah <mammarbellalah@gmail.com>
2024-12-02 05:57:32 -1100

    noued can

--- STM32CubeIDE/workspace_1.13.2/projetstm32-transmitter/Core/Src/main.c
+++ STM32CubeIDE/workspace_1.13.2/projetstm32-transmitter/Core/Src/main.c
@@ -33,13 +33,38 @@
 /* USER CODE BEGIN PD */
 uint8_t TxData_SOC[8] = {0x03, 0x58, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 uint8_t TxData_volt[8] = {0x03, 0x58, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-uint8_t TxData_temp[8] = {x00, 0x00, 0x19, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t TxData_temp[8] = {0x55, 0x00, 0x19, 0x00, 0x00, 0x00, 0x00, 0x00};
 uint8_t TxData_mos[8] = {0x04, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 uint8_t TxData_status[8] = {0x04, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-uint8_t TxData_cell_temp[8] = {0x03, 0x70, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+uint8_t TxData_cell_volt[16][8]={
+		{0x00, 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0x00},  // Frame 0
+		    {0x01, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x00},  // Frame 1
+		    {0x02, 0xAB, 0xCD, 0xEF, 0x01, 0x23, 0x45, 0x00},  // Frame 2
+		    {0x03, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x00},  // Frame 3
+		    {0x04, 0x70, 0x80, 0x90, 0xA0, 0xB0, 0xC0, 0x00},  // Frame 4
+		    {0x05, 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0x00},  // Frame 5
+		    {0x06, 0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x00},  // Frame 6
+		    {0x07, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x00},  // Frame 7
+		    {0x08, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x00},  // Frame 8
+		    {0x09, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x00},  // Frame 9
+		    {0x0A, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x00},  // Frame 10
+		    {0x0B, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x00},  // Frame 11
+		    {0x0C, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x00},  // Frame 12
+		    {0x0D, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x00},  // Frame 13
+		    {0x0E, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x00},  // Frame 14
+		    {0x0F, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x00}   // Frame 15
+};
+
+uint8_t TxData_cell_temp[3][8] = {
+    {0x00, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F, 0x30},  // Frame 0
+    {0x01, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37},  // Frame 1
+    {0x02, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E}   // Frame 2
+};
+
+
 uint8_t TxData_cell_balance[8] = {0x03, 0x70, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 uint8_t TxData_failuer[8] = {0x03, 0x70, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-
 
 
 uint8_t ID_soc = 0x90;
@@ -62,7 +87,7 @@ uint8_t IDR_status = 0x15;
 uint8_t IDR_cell_temp= 0x16;
 uint8_t IDR_cell_balance= 0x17;
 uint8_t IDR_failure= 0x18;
-
+uint8_t select = 0;
 
 uint8_t RxData[8];
 
@@ -130,16 +155,15 @@ int main(void)
   /* USER CODE BEGIN 2 */
    TxHeader.DLC = 8;
    TxHeader.IDE = CAN_ID_STD;
-   TxHeader.StdId = 0x10;//a changer
    TxHeader.RTR = CAN_RTR_DATA;
 
    sFilterConfig.FilterBank = 0;
    sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
    sFilterConfig.FilterScale = CAN_FILTERSCALE_16BIT;
-   sFilterConfig.FilterIdHigh = 0x10<< 5;
-   sFilterConfig.FilterMaskIdHigh =0xF0 << 5;
-   sFilterConfig.FilterIdLow =  0x10<< 5;
-   sFilterConfig.FilterMaskIdLow = 0xF0<< 5 ;
+   sFilterConfig.FilterIdHigh = 0x10<< 5  | (1 << 4);
+   sFilterConfig.FilterMaskIdHigh =0xF0 << 5 | (1 << 4);
+   sFilterConfig.FilterIdLow =  0x10<< 5 | (1 << 4);
+   sFilterConfig.FilterMaskIdLow = 0xF0<< 5  | (1 << 4);
    sFilterConfig.FilterFIFOAssignment = CAN_FILTER_FIFO0;
    sFilterConfig.FilterActivation = ENABLE;
 
@@ -149,7 +173,7 @@ int main(void)
 
    HAL_CAN_Start(&hcan1);
 
-   HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox) ;
+  // HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox) ;
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -159,6 +183,63 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  if (select == 1)
+	  {
+		        TxHeader.StdId = ID_soc ;
+		        HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData_SOC, &TxMailbox) ;
+		        select = 0 ;
+
+	  }else if (select == 2 )
+	  {
+	        TxHeader.StdId = ID_volt ;
+	        HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData_volt, &TxMailbox) ;
+	        select = 0 ;
+	  }
+	  else if (select == 3 )
+	  	  {
+	        TxHeader.StdId = ID_temp ;
+	        HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData_temp , &TxMailbox) ;
+	        select = 0 ;
+	  	  }
+	  else if (select == 4 )
+	  	  {
+	        TxHeader.StdId = ID_mos ;
+	        HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData_mos , &TxMailbox) ;
+	        select = 0 ;
+	  	  }
+	  else if (select == 5 )
+	  	  {
+	        TxHeader.StdId = ID_cell_volt ;
+	        for (int i = 0; i < 16; i++) {
+	        HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData_cell_volt[i] , &TxMailbox) ;
+	  	  }select = 0 ;}
+	  else if (select == 6 )
+	  	  {
+		  TxHeader.StdId = ID_status ;
+		  HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData_status , &TxMailbox) ;
+		  select = 0 ;
+	  	  }
+	  else if (select == 7 )
+	  	  {   TxHeader.StdId = ID_cell_temp ;
+		  for (int i = 0; i < 3; i++) {
+			  HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData_cell_temp[i], &TxMailbox) ;
+
+		    }
+		  select = 0 ;
+}
+	  else if (select == 8 )
+	  	  {
+		  TxHeader.StdId = ID_cell_balance ;
+		  HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData_cell_balance , &TxMailbox) ;
+		  select = 0 ;
+	  	  }
+	  else if (select == 9 )
+	  	  {
+		  TxHeader.StdId = ID_failure ;
+		  HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData_failuer , &TxMailbox) ;
+		  select = 0 ;
+	  	  }
+
   }
   /* USER CODE END 3 */
 }
@@ -266,8 +347,38 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
     if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData) != HAL_OK) {
+        if (RxHeader.StdId == IDR_soc) {
+					select = 1;
+				}
+        else if (RxHeader.StdId == IDR_volt) {
+					select = 2 ;
+				}
+        else if (RxHeader.StdId == IDR_temp) {
+                	select = 3 ;
+                }
+        else if (RxHeader.StdId == IDR_mos) {
+                	select = 4 ;
+                }
+        else if (RxHeader.StdId == IDR_cell_volt) {
+                	select = 5 ;
+                }
+        else if (RxHeader.StdId == IDR_status) {
+                	select = 6 ;
+                }
+        else if (RxHeader.StdId == IDR_cell_temp) {
+                	select = 7 ;
+                }
+        else if (RxHeader.StdId == IDR_cell_balance) {
+                	select = 8 ;
+                }
+        else if (RxHeader.StdId == IDR_failure) {
+                	select = 9 ;
+                }
+        else {
+        	select = 0 ;
+        }
 
-    	HAL_UART_Transmit(&huart1, RxData, sizeof(RxData), 100);
+    	//HAL_UART_Transmit(&huart1, RxData, sizeof(RxData), 100);
 
 
     	HAL_Delay(100);
